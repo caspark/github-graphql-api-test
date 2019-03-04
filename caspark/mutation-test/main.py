a65f60f15c68a4918763a3550de6814150856ffc
Caspar Krieger <git@asparck.com>
2019-03-03 19:33:16 -0800

    Autoformat python code

--- caspark/mutation-test/main.py
+++ caspark/mutation-test/main.py
@@ -1,7 +1,7 @@
 """
 A no-graphql-libraries implementation of fetching a repo by name and creating an issue in it with the current date & time.
 
-To run, do
+To run, do:
 pipenv install
 pipenv run python main-original.py
 """
@@ -12,9 +12,11 @@ import requests
 import os
 import pprint
 
-GITHUB_API_TOKEN = os.environ.get('GITHUB_API_TOKEN')
+GITHUB_API_TOKEN = os.environ.get("GITHUB_API_TOKEN")
 if not GITHUB_API_TOKEN:
-    raise ValueError("API token not specified; set the GITHUB_API_TOKEN environment variable to your Github API token")
+    raise ValueError(
+        "API token not specified; set the GITHUB_API_TOKEN environment variable to your Github API token"
+    )
 
 QUERY_FIND_REPO_ID = """
 query {
@@ -36,30 +38,34 @@ mutation($title: String!) {
 
 
 def graphql(query_or_mutation_string, variables={}):
-    body = json.dumps({
-        'query': query_or_mutation_string,
-        'variables': variables
-    })
+    body = json.dumps({"query": query_or_mutation_string, "variables": variables})
     print("Making request of %s" % body)
-    r = requests.post('https://api.github.com/graphql', data=body, headers={
-        'Content-Type': 'application/json',
-        'Accept': 'application/vnd.github.starfire-preview+json',
-        'Authorization': 'Bearer %s' % GITHUB_API_TOKEN
-    })
+    r = requests.post(
+        "https://api.github.com/graphql",
+        data=body,
+        headers={
+            "Content-Type": "application/json",
+            "Accept": "application/vnd.github.starfire-preview+json",
+            "Authorization": "Bearer %s" % GITHUB_API_TOKEN,
+        },
+    )
     r_json = r.json()
     print("JSON response was", pprint.pformat(r_json))
-    if r_json.get('errors'):
-        raise ValueError("GraphQL request error; error follows:\n%s" % pprint.pformat(r_json['errors']))
-    return r_json['data']
+    if r_json.get("errors"):
+        raise ValueError(
+            "GraphQL request error; error follows:\n%s"
+            % pprint.pformat(r_json["errors"])
+        )
+    return r_json["data"]
 
 
 def main():
-    repo_id = graphql(QUERY_FIND_REPO_ID)['repository']['id']
+    repo_id = graphql(QUERY_FIND_REPO_ID)["repository"]["id"]
     print("Found repo of", repo_id)
 
-    issue_url = graphql(MUTATION_CREATE_ISSUE, {
-        'title': 'Test issue: %s' % datetime.datetime.now()
-    })['createIssue']['issue']['url']
+    issue_url = graphql(
+        MUTATION_CREATE_ISSUE, {"title": "Test issue: %s" % datetime.datetime.now()}
+    )["createIssue"]["issue"]["url"]
     print("Made issue of", issue_url)
 
 
